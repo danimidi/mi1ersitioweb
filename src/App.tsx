f3bfad82fbcbb0b2699d4916ef81dfa255b44594
nelsimiramag <nelsi.miramag@hunty.com>
2024-06-03 23:33:29 -0500

    add fixes

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
     <Router basename='/mi1ersitioweb'>
       <Routes>
         <Route path='/mi1ersitioweb' element={<Landing />} />
-        <Route path='/mi1ersitioweb/login' element={<Login />} />
-        <Route path='/mi1ersitioweb/feed' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/feed' element={<Home />} />
       </Routes>
     </Router>
   );
